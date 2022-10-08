8e8070c6ab51cd304af6d6df3c7aa11c04377abe
NihalaBasith <nihalabasith@icloud.com>
2022-10-08 14:29:31 +0530

    templates created

--- home/views.py
+++ home/views.py
@@ -3,7 +3,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-    return HttpResponse("home page")
+    return render(request, 'index.html')
 
 def about(request):
     return HttpResponse("about page") 
